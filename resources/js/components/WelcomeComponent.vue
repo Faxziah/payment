2264496bd4986253dfe6bb0d5b0cc3f387baefe8
Avakyan Sasun <savakyan@lab4u.ru>
2023-12-17 13:18:12 +0400

    Initial commit, installed vue2 and vuetify2

---
+++ resources/js/components/WelcomeComponent.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Welcoze</h1>
+        <v-btn color="primary" class="my-2">qwe</v-btn>
+        <v-alert dismissable type="success">error</v-alert>
+    </div>
+</template>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+<div id="app">
+    <v-app>
+        <v-main>
+            <v-container>
+                <welcome-component></welcome-component>
+            </v-container>
+        </v-main>
+    </v-app>
+</div>
+
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
